d3e9596df07a3b7a659a30830008c784546ea870
a1852760 <a1852760@adelaide.edu.au>
2023-04-11 18:05:26 +0930

    prac5

--- main.cpp
+++ main.cpp
@@ -15,8 +15,27 @@ void printVector(vector<int> myVector) {
 
 int main() {
     QuickSort q1;
-    vector <int> original = {10,1,3,5,6,7,8};
+    RecursiveBinarySearch r1;
+    vector <int> original;
+    int num;
+    int num_length = 0;
+    while (cin >> num) {
+        original.push_back(num);
+        num_length++;
+    }
     vector <int> duplicate = q1.sort(original);
+
+    if (r1.search(duplicate, 1) == 1) {
+        cout << "true" << " ";
+    } else {
+        cout << "false" << " ";
+    }
+    int i = 0;
+    while (i < num_length) {
+        cout << duplicate.at(i) << " ";
+        i++;
+    }
+    cout << endl;
 
     return 0;
 }
