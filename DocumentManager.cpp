395448cd039a9a701accb50b6e5e320d5a6ddd96
a1852760 <a1852760@adelaide.edu.au>
2023-05-29 16:12:11 +0930

    add

--- DocumentManager.cpp
+++ DocumentManager.cpp
@@ -1,26 +1,59 @@
 #include "DocumentManager.h"
 #include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
 using namespace std;
 
-void DocumentManager::addDocument(string name, int id, int license_limit) {
+void DocumentManager::addDocument(string &name, int id, int license_limit)
+{
     documents[id] = {name, license_limit};
     borrowed_counts[id] = 0;
-};
+}
 
-void DocumentManager::addPatron(int patronID) {
+void DocumentManager::addPatron(int patronID)
+{
     patrons.insert(patronID);
-};
+}
 
-int DocumentManager::search(string name) {
-    for (const auto& pair : documents) {
-        if (pair.second.first == name) {
+int DocumentManager::search(string &name)
+{
+    for (const auto &pair : documents)
+    {
+        if (pair.second.first == name)
+        {
             return pair.first;
         }
     }
     return 0;
 }
 
+bool DocumentManager::borrowDocument(int docid, int patronID)
+{
+    if (patrons.find(patronID) == patrons.end())
+    {
+        return false;
+    }
 
-bool borrowDocument(int docid, int patronID); 
+    auto document = documents.find(docid);
+    if (document == documents.end())
+    {
+        return false;
+    }
 
-void returnDocument(int docid, int patronID);
+    if (borrowed_counts[docid] >= document->second.second)
+    {
+        return false;
+    }
+
+    borrowed_counts.at(docid)++;
+    return true;
+}
+
+void DocumentManager::returnDocument(int docid, int patronID)
+{
+    if (borrowed_counts[docid] > 0)
+    {
+        borrowed_counts.at(docid)--;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 #include "DocumentManager.h"
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
 
 int main() {
     DocumentManager manager;
-    manager.addDocument("Document 1", 1, 2);
-    manager.addDocument("Document 2", 2, 1);
-    manager.addDocument("Document 3", 3, 3);
-
-    manager.addPatron(101);
-    manager.addPatron(102);
-    manager.addPatron(103);
 
     return 0;
 }
